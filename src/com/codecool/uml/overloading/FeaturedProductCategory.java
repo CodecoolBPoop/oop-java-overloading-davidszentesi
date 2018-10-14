48c9c5fc34848e3be6371b5fad0db2872ba99a6e
David Szentesi <szntsd@gmail.com>
2018-10-14 15:18:29 +0200

    featuredProductCategory skeleton

--- src/com/codecool/uml/overloading/FeaturedProductCategory.java
+++ src/com/codecool/uml/overloading/FeaturedProductCategory.java
@@ -1,4 +1,19 @@
 package com.codecool.uml.overloading;
 
-public class FeaturedProductCategory {
+import java.util.Date;
+
+public class FeaturedProductCategory extends ProductCategory {
+    private Date startDate;
+    private Date endDate;
+
+    public void schedule(Date startDate, Date endDate) {
+        //TODO
+    }
+
+    @Override
+    public String toString() {
+        //TODO
+        return String;
+    }
+
 }
