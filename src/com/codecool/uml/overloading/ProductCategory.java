a68d9611557343821ce5f95238f513567f20dfc9
David Szentesi <szntsd@gmail.com>
2018-10-14 15:15:36 +0200

    productCategory skeleton

--- src/com/codecool/uml/overloading/ProductCategory.java
+++ src/com/codecool/uml/overloading/ProductCategory.java
@@ -1,4 +1,58 @@
 package com.codecool.uml.overloading;
 
+import java.util.List;
+
 public class ProductCategory {
+    private int id;
+    private String name;
+    private String department;
+    private String description;
+
+    public ProductCategory() {
+        //TODO
+    }
+
+    public ProductCategory(String name, String department, String description) {
+        //TODO
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProducts() {
+        //TODO
+        return productList;
+    }
+
+    @Override
+    public String toString() {
+        //TODO
+        return String;
+    }
+
 }
